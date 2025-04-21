d29f68a8f4e2f544202234262ce31f48f9e9f5eb
JefferDevop <somadesarrollo@gmail.com>
2025-04-21 13:28:49 -0500

    change hosting conexion.bar

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
-export const BASE_API = "https://meladitos.catalogointeractivo.com.co"
+export const BASE_API = "https://meladitos.conexion.bar"
 export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/"
